2a298b9659de49af66d029354a6535669ea67ef6
Caio Henrique de Sousa Guerreiro <78889968+CaioGuerreiro@users.noreply.github.com>
2026-04-13 12:51:34 -0300

    Add: rotting-fruit - submission-4

---
+++ Data Structures & Algorithms/rotting-fruit/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        fresh = 0
+        time = 0
+        queue = deque()
+
+        def get_neighbors(r,c):
+            if r < 0 or r == ROWS or c < 0 or c == COLS or grid[r][c] != 1:
+                return
+            nonlocal fresh
+            grid[r][c] = 2
+            fresh -= 1
+            queue.append([r, c])
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    queue.append([r,c])
+        if fresh == 0:
+            return 0
+
+        while queue:
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                get_neighbors(r+1, c)
+                get_neighbors(r-1, c)
+                get_neighbors(r, c+1)
+                get_neighbors(r, c-1)
+            if queue:
+                time += 1
+        return time if fresh == 0 else -1
+
