38e365feb0f223f10d94e83bcdfe32f60a41c825
Caio Henrique de Sousa Guerreiro <78889968+CaioGuerreiro@users.noreply.github.com>
2026-04-16 19:08:35 -0300

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+        def rob(self, nums: List[int]) -> int:
+            rob1, rob2 = 0, 0
+            # time O(n) space O(1)
+            for n in nums:
+                temp = max(n + rob1, rob2)
+                rob1 = rob2
+                rob2 = temp
+            return max(rob2,rob1)
+"""
+Input: nums = [1,1,3,3]
+                 
+
+temp = 1
+rob1 = 0
+rob2 = 0
+             
+"""
