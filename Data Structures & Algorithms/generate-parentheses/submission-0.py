dc9996c74cfba483db71f852f3d533a6d2256ebe
Caio Henrique de Sousa Guerreiro <78889968+CaioGuerreiro@users.noreply.github.com>
2026-04-17 18:37:03 -0300

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backtrack(openCount, closeCount):
+            if openCount == closeCount == n: #n é numero dado da questão
+                res.append("".join(stack))
+                return
+
+            if openCount < n:
+                stack.append('(')
+                backtrack(openCount + 1, closeCount)
+                stack.pop()
+            if closeCount < openCount:
+                stack.append(')')
+                backtrack(openCount, closeCount + 1)
+                stack.pop()
+        backtrack(0, 0)
+        return res
