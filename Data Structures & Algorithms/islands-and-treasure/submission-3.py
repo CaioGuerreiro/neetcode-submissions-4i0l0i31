5bfa3b864a056f0aa4b67a1dd00ab1626e9c92ce
Caio Henrique de Sousa Guerreiro <78889968+CaioGuerreiro@users.noreply.github.com>
2026-04-21 16:57:07 -0300

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        queue = deque()
+        visit = set()
+
+        def get_nei(r,c):
+            if r < 0 or r == ROWS or c < 0 or c == COLS or (r,c) in visit or grid[r][c] == -1:
+                return 
+            queue.append([r,c])
+            visit.add((r,c))
+            
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    queue.append([r,c])
+                    visit.add((r,c))
+        dist = 0
+        while queue:
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                grid[r][c] = dist
+                get_nei(r+1,c)
+                get_nei(r-1,c)
+                get_nei(r,c+1)
+                get_nei(r,c-1)
+            dist +=1 
